0ff331396a11683097e8c15fbf049f6b7ada9fd4
peterrw9000 <peterrw9001@yahoo.com>
2021-10-08 11:34:35 -0500

    Week 4 Platforming File UPDATE

---
+++ Week3Platforming/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int score = 0;
+    public int totalCoins = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score == totalCoins)
+        {
+            SceneManager.LoadScene(1);
+            score = 0;
+        }
+    }
+}
